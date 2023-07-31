3c10ab5d17b985aee3d2aa4d820b8598954ca5d5
Andy Khau <fakeemail1441@gmail.com>
2023-07-30 21:34:30 -0400

    Start map functionality

---
+++ src/components/worldMap.js
@@ -0,0 +1,17 @@
+"use client"
+
+import React from "react";
+import { ComposableMap} from "react-simple-maps";
+
+const WorldMap = () => {
+
+    returm (
+        <ComposableMap
+            projection={"geoAlbers"}
+        >
+
+        </ComposableMap>
+    )
+}
+
+export default WorldMap
