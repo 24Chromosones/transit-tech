97501dfdaeff79cf9dd0e452bab7a9e44096a142
Andy Khau <fakeemail1441@gmail.com>
2023-08-01 13:04:59 -0400

    Improve efficiency for LLM agent

--- api/gpt_csv.py
+++ api/gpt_csv.py
@@ -1,15 +1,11 @@
 import os
-import tempfile
 import uuid
 
-
-import pandas as pd
 from dotenv import load_dotenv
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, session, jsonify, current_app
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from waitress import serve
-
 
 secret_key = uuid.uuid4().hex
 
@@ -23,6 +19,7 @@ app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
 @app.route("/api/test")
 def test():
     return "<p>test<p>"
+
 
 # FIXME: Currently, this is dangerous as there is no security between different sessions
 @app.route("/api/upload-csv", methods=['POST'])
@@ -45,40 +42,30 @@ def receive_file():
     session['agent'] = None
     file.save(file_path)
 
+    load_dotenv()
+    os.getenv("OPENAI_API_KEY")
+
+    current_app.agent = create_csv_agent(
+        OpenAI(temperature=0),
+        file_path,
+        max_iterations=10000,
+    )
+    print("agent created", flush=True)
+
     return "received", 200
 
 
 @app.route("/api/ask-gpt", methods=['POST'])
 def ask_gpt():
     if request.json['question'] == 'test':
-        return jsonify(answer='testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words ')
+        return jsonify(
+            answer='testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words testing alot of words ')
 
-    load_dotenv()
-    os.getenv("OPENAI_API_KEY")
-
-    csv_path = session.get('file_path')
-
+    agent = current_app.agent
 
     content = request.json
     content = content['question']
-    print(str(csv_path), flush=True)
     print(content, flush=True)
-
-    # if session['agent'] is None:
-    #     agent = create_csv_agent(
-    #         OpenAI(temperature=0),
-    #         csv_path,
-    #     )
-    #     session['agent'] = 'agent'
-    #     print("agent created", flush=True)
-
-    agent = create_csv_agent(
-        OpenAI(temperature=0),
-        csv_path,
-        max_iterations=10000,
-    )
-    # session['agent'] = 'agent'
-    print("agent created", flush=True)
 
     if content is not None and content != "":
         return jsonify(answer=agent.run(content))
