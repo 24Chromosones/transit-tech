1a6e280218688cf8fcfdd7826516bc7a21b777d9
Andy Khau <fakeemail1441@gmail.com>
2023-08-04 21:33:14 -0700

    Archive website

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
     return (
         <main className={styles.page}>
             <h1 className={styles.title}>Transit Tech</h1>
+            <h1 className={styles.announcement}>Due to the high API cost, the website is currently archived.</h1>
             {/*<WorldMap latitude={34.052490234375} longitude={-118.24370574951172} />*/}
 
 
